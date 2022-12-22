d4b05e69eb3cf430f67c396a66430e9b7b89f703
kirich9k <kirya65@gmail.com>
2022-12-22 19:58:20 +0300

    Task 38 accomplished

--- Task38/Program.cs
+++ Task38/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void InputArray(int[]array){
+	for (int i = 0; i < array.Length; i++)
+		array[i] = new Random().Next(0, 1000);
+}
+
+void ReleaseArray(int[] array){
+    int different = 0;
+    int min = 1000;
+    int max = 0; 
+	for (int i = 0; i < array.Length; i++){
+		if (array[i] > max)
+			max = array[i];          
+	}
+    for (int i = 0; i < array.Length; i++){
+		if (array[i] < min)
+			min = array[i];          
+	}
+    different = max - min;
+	Console.WriteLine($"Разница между максимальным и минимальным: {different}");
+}
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+ReleaseArray(array);
