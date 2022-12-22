6af68848c6ad21b869867a2a93b87c23ac9fd138
kirich9k <kirya65@gmail.com>
2022-12-22 19:35:58 +0300

    Task 34 and 36 was accomplished

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void InputArray(int[]array){
+	for (int i = 0; i < array.Length; i++)
+		array[i] = new Random().Next(100, 1000);
+}
+
+void ReleaseArray(int[] array){
+	int sumEven = 0;
+	for (int i = 0; i < array.Length; i++){
+		if (array[i] % 2 == 0)
+			sumEven = sumEven + 1;
+	}
+	Console.WriteLine($"Кол-во чётных чисел: {sumEven}");
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+ReleaseArray(array);
--- Task36/Program.cs
+++ Task36/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void InputArray(int[]array){
+	for (int i = 0; i < array.Length; i++)
+		array[i] = new Random().Next(0, 10);
+}
+
+void ReleaseArray(int[] array){
+	int sumEvenPositions = 0;
+	for (int i = 0; i < array.Length; i++){
+		if (i % 2 == 0)
+			sumEvenPositions = array[i] + sumEvenPositions;
+	}
+	Console.WriteLine($"Сумма элементов, стоящих на нечетных позициях: {sumEvenPositions}");
+}
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+ReleaseArray(array);
